ada94ecc0c6277f9c4aae16cc2c0408fada95de7
allessoep <57958153+allessoep@users.noreply.github.com>
2021-06-24 15:30:17 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+// Horizontal Scrolling Using Mouse Wheel
+// Code From: www.dte.web.id/2013/02/event-mouse-wheel.html
+(function() {
+function scrollHorizontally(e) {
+e = window.event || e;
+var delta = Math.max(-0.5, Math.min(0.5, (e.wheelDelta || -e.detail)));
+document.documentElement.scrollLeft -= (delta*50); // Multiplied by 40
+document.body.scrollLeft -= (delta*50); // Multiplied by 40
+e.preventDefault();
+}
+if (window.addEventListener) {
+// IE9, Chrome, Safari, Opera
+window.addEventListener("mousewheel", scrollHorizontally, false);
+// Firefox
+window.addEventListener("DOMMouseScroll", scrollHorizontally, false);
+} else {
+// IE 6/7/8
+window.attachEvent("onmousewheel", scrollHorizontally);
+}
+})();
