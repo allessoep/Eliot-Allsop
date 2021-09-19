6b5826d18ad0b87f695f85fb4cb83ae68887018a
allessoep <57958153+allessoep@users.noreply.github.com>
2021-09-19 23:44:35 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -23,15 +23,18 @@ window.attachEvent("onmousewheel", scrollHorizontally);
 
 
 
-
 //flexbox 1 event listeners
 
 //high fidelity
 document.getElementById( "HighFidelity" ).onclick= function(){
 document.getElementById('HighFidelitysound').play();
+let str = document.getElementById("test").innerHTML;
+document.getElementById("test").innerHTML =  "High Fidelity 17.5 x 16 cm";
 };
 document.getElementById( "HighFidelity" ).ontouchstart = function(){
 document.getElementById('HighFidelitysound').play();
+let str = document.getElementById("test").innerHTML;
+document.getElementById("test").innerHTML =  "high fidelity 17.5 x 16 cm";
 };
 
 //PupulaDuplex
@@ -45,9 +48,13 @@ document.getElementById('PupulaDuplexsound').play();
 //Pod
 document.getElementById( "Pod" ).onclick= function(){
 document.getElementById('Podsound').play();
+let str = document.getElementById("test").innerHTML;
+document.getElementById("test").innerHTML =  "Pod 15.5 x 20 cm";
 };
 document.getElementById( "Pod" ).ontouchstart = function(){
 document.getElementById('Podsound').play();
+let str = document.getElementById("test").innerHTML;
+document.getElementById("test").innerHTML =  "pod 15.5 x 20 cm";
 };
 
 //Hypnogogicstate
