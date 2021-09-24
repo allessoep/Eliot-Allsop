8bd408046ed23e78344dc0bf73ac1aafe48a11d4
allessoep <57958153+allessoep@users.noreply.github.com>
2021-09-24 12:54:36 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -184,57 +184,85 @@ document.getElementById("description").innerHTML =  "Elevated Section <br> 12 x 
 //Nightschool
 document.getElementById( "Nightschool" ).onclick= function(){
 document.getElementById('Nightschoolsound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "Night School <br> 16.5 x 17 cm";
 };
 document.getElementById( "PupulaDuplex" ).ontouchstart = function(){
 document.getElementById('PupulaDuplexsound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "Night School <br> 16.5 x 17 cm";
 };
 
 //BunkerHill
 document.getElementById( "BunkerHill" ).onclick= function(){
 document.getElementById('BunkerHillsound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "Bunker Hill <br> 23 x 18 cm";
 };
 document.getElementById( "BunkerHill" ).ontouchstart = function(){
 document.getElementById('BunkerHillsound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "Bunker Hill <br> 23 x 18 cm";
 };
 
 //PodIII
 document.getElementById( "PodIII" ).onclick= function(){
 document.getElementById('PodIIIsound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "Pod III <br> 9 x 10.5 cm";
 };
 document.getElementById( "PodIII" ).ontouchstart = function(){
 document.getElementById('PodIIIsound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "Pod III <br> 9 x 10.5 cm";
 };
 
 //MimeticDesires
 document.getElementById( "MimeticDesires" ).onclick= function(){
 document.getElementById('MimeticDesiressound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "Mimetic Desires <br> 16 x 12 cm";
 };
 document.getElementById( "MimeticDesires" ).ontouchstart = function(){
 document.getElementById('MimeticDesiressound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "Mimetic Desires <br> 16 x 12 cm";
 };
 
 //Cyberiad
 document.getElementById( "Cyberiad" ).onclick= function(){
 document.getElementById('Cyberiadsound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "Cyberiad <br> 13 x 13 cm";
 };
 document.getElementById( "Cyberiad" ).ontouchstart = function(){
 document.getElementById('Cyberiadsound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "Cyberiad <br> 13 x 13 cm";
 };
 
 //EventHorizon
 document.getElementById( "EventHorizon" ).onclick= function(){
 document.getElementById('EventHorizonsound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "Event Horizon <br> 13.5 x 13.5 cm";
 };
 document.getElementById( "PupulaDuplex" ).ontouchstart = function(){
 document.getElementById('PupulaDuplexsound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "Event Horizon <br> 13.5 x 13.5 cm";
 };
 
 //CodedSleep
 document.getElementById( "CodedSleep" ).onclick= function(){
 document.getElementById('CodedSleepsound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "Coded Sleep <br> 11 x 8.5 cm";
 };
 document.getElementById( "CodedSleep" ).ontouchstart = function(){
 document.getElementById('CodedSleepsound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "Coded Sleep <br> 11 x 8.5 cm";
 };
 
 //Arachne
@@ -252,17 +280,25 @@ document.getElementById("description").innerHTML =  "Arachne <br> 11.5 x 13 cm";
 //PatternRecognition
 document.getElementById( "PatternRecognition" ).onclick= function(){
 document.getElementById('PatternRecognitionsound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "Pattern Recognition <br> 13 x 13.5 cm";
 };
 document.getElementById( "PatternRecognition" ).ontouchstart = function(){
 document.getElementById('PatternRecognitionsound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "Pattern Recognition <br> 13 x 13.5 cm";
 };
 
 //SlowCancellation
 document.getElementById( "SlowCancellation" ).onclick= function(){
 document.getElementById('SlowCancellationsound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "The Slow Cancellation of the Future <br> 10 x 15 cm";
 };
 document.getElementById( "SlowCancellation" ).ontouchstart = function(){
 document.getElementById('SlowCancellationsound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "The Slow Cancellation of the Future <br> 10 x 15 cm";
 };
 
 
@@ -272,137 +308,205 @@ document.getElementById('SlowCancellationsound').play();
 //Prometheos
 document.getElementById( "Prometheos" ).onclick= function(){
 document.getElementById('Prometheossound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "Prometheos <br> 16.5 x 12 cm";
 };
 document.getElementById( "Prometheos" ).ontouchstart = function(){
 document.getElementById('Prometheossound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "Prometheos <br> 16.5 x 12 cm";
 };
 
 //SentientTumor
 document.getElementById( "SentientTumor" ).onclick= function(){
 document.getElementById('SentientTumorsound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "Sentient Tumor <br> 13.5 x 15.5 cm";
 };
 document.getElementById( "SentientTumor" ).ontouchstart = function(){
 document.getElementById('SentientTumorsound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "Sentient Tumor <br> 13.5 x 15.5 cm";
 };
 
 //TheArchitectureoftheSky
 document.getElementById( "TheArchitectureoftheSky" ).onclick= function(){
 document.getElementById('TheArchitectureoftheSkysound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "The Architecture of the Sky <br> 22.5 x 17 cm";
 };
 document.getElementById( "TheArchitectureoftheSky" ).ontouchstart = function(){
 document.getElementById('TheArchitectureoftheSkysound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "The Architecture of the Sky <br> 22.5 x 17 cm";
 };
 
 //Trypophobia
 document.getElementById( "Trypophobia" ).onclick= function(){
 document.getElementById('Trypophobiasound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "Trypophobia <br> 17 x 13 cm";
 };
 document.getElementById( "Trypophobia" ).ontouchstart = function(){
 document.getElementById('Trypophobiasound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "Trypophobia <br> 17 x 13 cm";
 };
 
 //Uncannyvalley
 document.getElementById( "Uncannyvalley" ).onclick= function(){
 document.getElementById('Uncannyvalleysound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "Uncanny Valley <br> 17.5 x 13.5 cm";
 };
 document.getElementById( "Uncannyvalley" ).ontouchstart = function(){
 document.getElementById('Uncannyvalleysound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "Uncanny Valley <br> 17.5 x 13.5 cm";
 };
 
 //Revolutionforthedead
 document.getElementById( "Revolutionforthedead" ).onclick= function(){
 document.getElementById('Revolutionforthedeadsound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "Revolution for the Dead <br> 13.5 x 16.5 cm";
 };
 document.getElementById( "Revolutionforthedead" ).ontouchstart = function(){
 document.getElementById('Revolutionforthedeadsound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "Revolution for the Dead <br> 13.5 x 16.5 cm";
 };
 
 //Cosmophobia
 document.getElementById( "Cosmophobia" ).onclick= function(){
 document.getElementById('Cosmophobiasound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "Cosmophobia <br> 20 x 13 cm";
 };
 document.getElementById( "Cosmophobia" ).ontouchstart = function(){
 document.getElementById('Cosmophobiasound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "Cosmophobia <br> 20 x 13 cm";
 };
 
 //FilthyMagic
 document.getElementById( "FilthyMagic" ).onclick= function(){
 document.getElementById('FilthyMagicsound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "Filthy Magic <br> 16 x 10 cm";
 };
 document.getElementById( "FilthyMagic" ).ontouchstart = function(){
 document.getElementById('FilthyMagicsound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "Filthy Magic <br> 16 x 10 cm";
 };
 
 //DigitalPathologies
 document.getElementById( "DigitalPathologies" ).onclick= function(){
 document.getElementById('DigitalPathologiessound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "Digital Pathologies <br> 16 x 16.5 cm";
 };
 document.getElementById( "DigitalPathologies" ).ontouchstart = function(){
 document.getElementById('DigitalPathologiessound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "Digital Pathologies <br> 16 x 16.5 cm";
 };
 
 //ExtremeMetaphors
 document.getElementById( "ExtremeMetaphors" ).onclick= function(){
 document.getElementById('ExtremeMetaphorssound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "Extreme Metaphors <br> 11 x 17 cm";
 };
 document.getElementById( "ExtremeMetaphors" ).ontouchstart = function(){
 document.getElementById('ExtremeMetaphorssound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "Extreme Metaphors <br> 11 x 17 cm";
 };
 
 //Memoriesofthefuture
 document.getElementById( "Memoriesofthefuture" ).onclick= function(){
 document.getElementById('Memoriesofthefuturesound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "Memories of the Future <br> 23 x 15 cm";
 };
 document.getElementById( "Memoriesofthefuture" ).ontouchstart = function(){
 document.getElementById('Memoriesofthefuturesound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "Memories of the Future <br> 23 x 15 cm";
 };
 
 //Isopoda
 document.getElementById( "Isopoda" ).onclick= function(){
 document.getElementById('Isopodasound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "Isopoda <br> 10 x 14 cm";
 };
 document.getElementById( "Isopoda" ).ontouchstart = function(){
 document.getElementById('Isopodasound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "isopoda <br> 10 x 14 cm";
 };
 
 //TheNewEros
 document.getElementById( "TheNewEros" ).onclick= function(){
 document.getElementById('TheNewErossound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "The New Eros <br> 6.5 x 5 cm";
 };
 document.getElementById( "TheNewEros" ).ontouchstart = function(){
 document.getElementById('TheNewErossound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "The New Eros <br> 6.5 x 5 cm";
 };
 
 //TheAviatrix
 document.getElementById( "TheAviatrix" ).onclick= function(){
 document.getElementById('TheAviatrixsound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "The Aviatrix <br> 14 x 14.5 cm";
 };
 document.getElementById( "TheAviatrix" ).ontouchstart = function(){
 document.getElementById('TheAviatrixsound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "The Aviatrix <br> 14 x 14.5 cm";
 };
 
 //Thelasttransitofvenus
 document.getElementById( "Thelasttransitofvenus" ).onclick= function(){
 document.getElementById('Thelasttransitofvenussound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "The Last Transit of Venus <br> 12 x 12 cm";
 };
 document.getElementById( "Thelasttransitofvenus" ).ontouchstart = function(){
 document.getElementById('Thelasttransitofvenussound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "The Last Transit of Venus <br> 12 x 12 cm";
 };
 
 //SongsofInnocence
 document.getElementById( "SongsofInnocence" ).onclick= function(){
 document.getElementById('SongsofInnocencesound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "Songs of Innocence <br> 11 x 9.5 cm";
 };
 document.getElementById( "SongsofInnocence" ).ontouchstart = function(){
 document.getElementById('SongsofInnocencesound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "Songs of Innocence <br> 11 x 9.5 cm";
 };
 
 //TheThoughtCollective
 document.getElementById( "TheThoughtCollective" ).onclick= function(){
 document.getElementById('TheThoughtCollectivesound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "The Thought Collective <br> 20 x 14 cm";
 };
 document.getElementById( "TheThoughtCollective" ).ontouchstart = function(){
 document.getElementById('TheThoughtCollectivesound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "The Thought Collective <br> 20 x 14 cm";
 };
 
 //TheSchedule
@@ -420,9 +524,13 @@ document.getElementById("description").innerHTML =  "The Schedule of Addiction <
 //CrisisCult
 document.getElementById( "CrisisCult" ).onclick= function(){
 document.getElementById('CrisisCultsound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "Crisis Cult <br> 23 x 11 cm";
 };
 document.getElementById( "CrisisCult" ).ontouchstart = function(){
 document.getElementById('CrisisCultsound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "Crisis Cult <br> 23 x 11 cm";
 };
 
 //DarkEuphoria
@@ -440,7 +548,11 @@ document.getElementById("description").innerHTML =  "Dark Euphoria <br> 19.5 x 1
 //ladyLake
 document.getElementById( "LadyLake" ).onclick= function(){
 document.getElementById('LadyLakesound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "The Lady of the Lake <br> 10.5 x 11 cm";
 };
 document.getElementById( "LadyLake" ).ontouchstart = function(){
 document.getElementById('LadyLakesound').play();
+let str = document.getElementById("description").innerHTML;
+document.getElementById("description").innerHTML =  "The Lady of the lake <br> 10.5 x 11 cm";
 };
